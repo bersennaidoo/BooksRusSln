030374981224dc84e446256bb1d6f7afecbc8708
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-02-27 14:33:01 +0200

    Implemented AuthorController List action thats builds a view model from the List actions parameters for sorting and paging by the repository and passed to the  Author List view for rendering html and styled view with bootstrap 5 class

---
+++ BooksRus/Controllers/AuthorController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using BooksRus.Models;
+
+namespace BooksRus.Controllers;
+
+public class AuthorController : Controller
+{
+    private Repository<Author> data { get; set; }
+
+    public AuthorController(BookstoreContext ctx) => data = new Repository<Author>(ctx);
+
+    public IActionResult Index() => RedirectToAction("List");
+
+    public ViewResult List(AuthorGridData values)
+    {
+        // Contructs a QueryOptions thats operates on an Author for the Repository instance data.
+        var options = new QueryOptions<Author> 
+        {
+            Includes = "Books",
+            PageNumber = values.PageNumber,
+            PageSize = values.PageSize,
+            OrderByDirection = values.SortDirection
+        };
+
+        // Checks if values IsSortByFirstName holds or not holds then assigns lambda to options OrderBy delegate.
+        if (values.IsSortByFirstName)
+            options.OrderBy = a => a.FirstName;
+        else
+            options.OrderBy = a => a.LastName;
+
+        // Creates a view model with an instance of the AuthorGridData Authors for the list view
+        // to build the route links for sorting and paging that Repository uses to OrderBy.
+        var vm = new AuthorListViewModel
+        {
+            Authors = data.List(options),
+            CurrentRoute = values,
+            TotalPages = values.GetTotalPages(data.Count)
+        };
+
+        return View(vm);
+    }
+}
+
+
